a95f22b4c3a10888fcc321c9c8a04e8f9f545033
Jesse Freeman <jessefreeman@gmail.com>
2023-06-13 07:09:59 -0700

    Final code for lesson 2.2

---
+++ HelloNoteApp/Note.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace HelloNoteApp
+{
+	public class Note
+	{
+		public int Id { get; set; }
+		public string Title { get; set; }
+		public string Content { get; set; }
+	}
+}
+
